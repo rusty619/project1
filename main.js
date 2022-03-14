e141b8416ff31755f734dcf511e0e5892baf3fbd
rusty619 <rusty6916@gmail.com>
2022-03-14 14:34:45 -0400

    ADDED JS EVENT

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 alert("JavaScript is running!!")
+
+let btn = document.getElementById('btn')
+// noSpaceInTHeWord with a capital Letter every word is camel case
+btn .addEventListener('click', () => {
+    alert("Clicked!!")
+})
